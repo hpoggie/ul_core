2b7fe28a9d07173b36a0d77e7c3af902dda96e52
hpoggie <henry.poggie@gmail.com>
2018-12-20 08:07:16 -0500

    Move availableFactions into ul_core

--- factions/__init__.py
+++ factions/__init__.py
@@ -1,2 +1,11 @@
 import sys
 sys.path.insert(0, "..")
+
+from . import templars
+from . import thieves
+from . import mariners
+from . import fae
+
+availableFactions = [
+    templars.Templar, mariners.Mariner, thieves.Thief, fae.Faerie
+]
