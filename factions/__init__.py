654ced8b3ae03f9f517a8607f3f2cdcca87b5a49
hpoggie <henry.poggie@gmail.com>
2018-12-20 16:49:05 -0500

    Add base to allCards

--- factions/__init__.py
+++ factions/__init__.py
@@ -1,6 +1,7 @@
 import sys
 sys.path.insert(0, "..")
 
+from . import base
 from . import templars
 from . import thieves
 from . import mariners
@@ -12,6 +13,6 @@ availableFactions = [
 
 allCards = {}
 
-for module in (templars, mariners, thieves, fae):
+for module in (base, templars, mariners, thieves, fae):
     for card in module.allCards:
         allCards[card.__name__] = card
