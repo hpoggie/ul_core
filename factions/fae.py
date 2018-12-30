baf2c03ac9134d1a5a2578e9a76eb29bb64826bb
hpoggie <henry.poggie@gmail.com>
2018-12-29 22:50:35 -0500

    Fae balance changes

--- factions/fae.py
+++ factions/fae.py
@@ -37,10 +37,10 @@ class titaniasGuard(Card):
     image = 'batwing-emblem.png'
     cost = 4
     rank = 4
-    desc = "When this spawns, turn target face-up unit face-down."
+    desc = "On Spawn: You may turn target face-up unit face-down."
 
     def onSpawn(self, target):
-        if target is None or not target.faceup or target.spell:
+        if not target.faceup or target.spell:
             raise InvalidTargetError()
 
         target.zone = target.controller.facedowns
@@ -71,12 +71,12 @@ class faerieDragon(Card):
     image = 'chameleon-glyph.png'
     cost = 5
     rank = 4
-    desc = ("If this would be destroyed while face-up, return it to its "
-            "owner's hand instead.")
+    desc = ("If this would be destroyed while face-up, turn it face-down "
+            "instead.")
 
     def moveToZone(self, zone):
         if self.faceup and zone is self.owner.graveyard:
-            super().moveToZone(self.owner.hand)
+            super().moveToZone(self.controller.facedowns)
         else:
             super().moveToZone(zone)
 
@@ -84,25 +84,29 @@ class faerieDragon(Card):
 class mesmerism(Card):
     name = "Mesmerism"
     image = 'night-vision.png'
-    cost = 2
+    cost = 10
     rank = 'il'
-    desc = "Destroy all your opponent's face-up units."
+    desc = "Gain control of all your opponent's face-up units."
 
     def onSpawn(self):
-        self.controller.opponent.faceups.destroyAllUnits()
+        for c in self.controller.opponent.faceups[:]:
+            c.zone = self.controller.faceups
 
 
 class returnToSender(Card):
     name = "Return to Sender"
     image = 'return-arrow.png'
-    cost = 3
+    cost = 9
     rank = 'il'
-    desc = "Return all face-down cards to their owners' hands."
+    desc = ("Return all enemy face-up units and face-down cards to their "
+            "owners' hands.")
 
     def onSpawn(self):
-        for pl in self.game.players:
-            for fd in pl.facedowns[:]:
-                fd.zone = fd.owner.hand
+        for fd in self.controller.opponent.facedowns[:]:
+            fd.zone = fd.owner.hand
+
+        for c in self.controller.opponent.faceups[:]:
+            c.zone = c.owner.hand
 
 
 class enchantersTrap(Card):
@@ -166,7 +170,7 @@ class Faerie(Player):
     cardBack = "fairy.png"
     deck = deck(
         faerieMoth, 5,
-        oberonsGuard, 4,
+        oberonsGuard, 2,
         titaniasGuard, 2,
         preciseDiscard, 2,
         mesmerism, 1,
