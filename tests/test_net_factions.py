9de145ace3ac23e4dcdefae3cf5b5161350133d9
Henry Poggie <henry.poggie@gmail.com>
2019-03-02 18:03:46 -0500

    Add net Templar

--- tests/test_net_factions.py
+++ tests/test_net_factions.py
@@ -2,6 +2,20 @@ from .util import newGame
 import ul_core.net.net_factions as nf
 
 
+def testTemplarAbilityIndices():
+    game, p0, p1 = newGame(nf.Templar)
+    game.start()
+    
+    try:
+        p0.factionAbility(100)  # index way too big
+    except nf.ProtocolError:
+        pass
+    else:
+        assert False
+
+    p0.factionAbility(0)
+
+
 def testFactionAbilityIndices():
     game, p0, p1 = newGame(nf.Thief)
 
--- ul_core/net/net_factions.py
+++ ul_core/net/net_factions.py
@@ -1,8 +1,19 @@
+import ul_core.factions.templars as templars
 import ul_core.factions.thieves as thieves
 
 
 class ProtocolError(Exception):
     pass
+
+
+class Templar(templars.Templar):
+    def factionAbility(self, discardIndex):
+        try:
+            discard = self.hand[discardIndex]
+        except IndexError as e:
+            raise ProtocolError("Bad discard index.", e)
+
+        self.templarAbility(discard)
 
 
 class Thief(thieves.Thief):
