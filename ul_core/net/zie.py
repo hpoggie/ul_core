56ae86ead73d6667726f3c6eb768a846a7904a93
Henry Poggie <henry.poggie@gmail.com>
2019-05-22 10:44:53 -0400

    Add zieToGameEntity and zieToCard

--- ul_core/net/zie.py
+++ ul_core/net/zie.py
@@ -12,12 +12,33 @@ def gameEntityToZie(player, entity):
         return cardToZie(player, entity)
 
 
+def zieToGameEntity(player, zie):
+    z, i, e = zie
+    # TODO: be more strict, check if zie == (-1, -1, 0)
+    # Using this for now for compatibility reasons
+    if z == -1:
+        return None
+    elif z == Zone.face:
+        return player.opponent.face if e else player.face
+    else:
+        return zieToCard(player, z, i, e)
+
+
 def cardToZie(player, card):
     """
     Convert a card to Zone, Index, Enemy representation
     """
     return (card.controller.zones.index(card.zone), card.zone.index(card),
             card.controller is player.opponent)
+
+
+def zieToCard(pl, targetZone, targetIndex, targetsEnemy):
+    if targetsEnemy:
+        target = pl.opponent.zones[targetZone][targetIndex]
+    else:
+        target = pl.zones[targetZone][targetIndex]
+
+    return target
 
 
 def playerFace():
